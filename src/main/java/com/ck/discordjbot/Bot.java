c926ee42c28a23992904729493349243d3e20da7
Csaba Koncz <ckoncz@cloudera.com>
2021-04-18 09:51:00 +0200

    initial commit

---
+++ src/main/java/com/ck/discordjbot/Bot.java
@@ -0,0 +1,45 @@
+
+package com.ck.discordjbot;
+
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.entities.Activity;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageChannel;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.requests.GatewayIntent;
+
+public class Bot extends ListenerAdapter
+{
+    public static void main(String[] args) throws LoginException
+    {
+        if (args.length < 1) {
+            System.out.println("You have to provide a token as first argument!");
+            System.exit(1);
+        }
+        // args[0] should be the token
+        // We only need 2 intents in this bot. We only respond to messages in guilds and private channels.
+        // All other events will be disabled.
+        JDABuilder.createLight(args[0], GatewayIntent.GUILD_MESSAGES, GatewayIntent.DIRECT_MESSAGES)
+            .addEventListeners(new Bot())
+            .setActivity(Activity.playing("Type !ping"))
+            .build();
+    }
+    
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event)
+    {
+        Message msg = event.getMessage();
+        if (msg.getContentRaw().equalsIgnoreCase("ping"))
+        {
+            MessageChannel channel = event.getChannel();
+            long time = System.currentTimeMillis();
+            channel.sendMessage("Pong!") /* => RestAction<Message> */
+                   .queue(response /* => Message */ -> {
+                       response.editMessageFormat("Pong: %d ms", System.currentTimeMillis() - time).queue();
+                   });
+        }
+    }
+}
